16507453e087c509fdc3ce83d78d6c5685f21d5c
赵天明 <@DESKTOP-KBFDARK>
2019-05-14 11:58:07 +0800

    wqe

---
+++ Model/Tourist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Tourist
+    {
+        public int TouristId { get; set; }
+        public string TourisName { get; set; }
+        public bool TourisSex { get; set; }
+        public string IdentityCard { get; set; }
+        public string TourisPhone { get; set; }
+    }
+}
