366f2f9a9fca98eb6e5d3039f4e92d3c44acee91
8615910461621 <1925618905@qq.com>
2019-05-15 21:07:30 +0800

    HYK

--- WebAPITravel/Controllers/ShowMonthController.cs
+++ WebAPITravel/Controllers/ShowMonthController.cs
@@ -17,6 +17,7 @@ namespace WebAPITravel.Controllers
         /// <returns></returns>
         public List<Monthes> ShowMonthes()
         {
+
             return bll.ShowMonthes();
         }
     }
